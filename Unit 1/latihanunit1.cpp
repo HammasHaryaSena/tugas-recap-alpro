f9befe556f8aa0cf095b75927f1bdf3e541a32f3
HammasHaryaSena <162069949+HammasHaryaSena@users.noreply.github.com>
2024-03-05 22:59:35 +0700

    Add files via upload

---
+++ Unit 1/latihanunit1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int a,b,c,d,e,f;
+float g,h,i;
+
+int main(){
+    scanf("%d",a);
+    scanf("%d",b);
+    scanf("%d",c);
+    scanf("%d",d);
+    scanf("%d",e);
+    scanf("%d",f);
+    g=a/b;
+    h=c*d;
+    i=e/f;
+    printf("%d",g);
+    return 0;
+}
+
