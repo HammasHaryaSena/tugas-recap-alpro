65715d90070e5b7be451c5df4c9cdd74ef85776f
HammasHaryaSena <162069949+HammasHaryaSena@users.noreply.github.com>
2024-03-05 23:16:45 +0700

    latihanunit1.cpp

--- Unit 1/latihanunit1.cpp
+++ Unit 1/latihanunit1.cpp
@@ -1,21 +1,33 @@
 #include <stdio.h>
 
-int a,b,c,d,e,f;
-float g,h,i;
-
 int main(){
-    scanf("%d",a);
-    scanf("%d",b);
-    scanf("%d",c);
-    scanf("%d",d);
-    scanf("%d",e);
-    scanf("%d",f);
-    g=a/b;
-    h=c*d;
-    i=e/f;
-    printf("%d",g);
-    printf("%d",h);
-    printf("%d",i);
+    int a,b,c,d,e,f;
+float q,w,r,t,y,u;
+float g,h,i;
+printf("masukan angka");
+    scanf("%d",&a);
+    printf("masukan angka");
+    scanf("%d",&b);
+    printf("masukan angka");
+    scanf("%d",&c);
+    printf("masukan angka");
+    scanf("%d",&d);
+    printf("masukan angka");
+    scanf("%d",&e);
+    printf("masukan angka");
+    scanf("%d",&f);
+    q=a*1.0;
+    w=b*1.0;
+    r=c*1.0;
+    t=d*1.0;
+    y=e*1.0;
+    u=f*1.0;
+    g=q/w;
+    h=r*t;
+    i=y/u;
+    printf("%f",g);
+    printf("%f",h);
+    printf("%f",i);
     return 0;
 }
 
